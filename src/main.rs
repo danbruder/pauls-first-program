4df5f7cbc72561c353e0b6249d57f488fd8ce965
Dan Bruder <ddbruder@gmail.com>
2020-05-23 09:09:23 -0400

    ini

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, Paul, let's do some math! \n(this is Adelynn too, we are taking turns!)");
+
+    let x: i128 = 123456789 + 123456789;
+    println!("\t -----> The answer is {:?}", &x);
+}
